219076a70e93bf2174b16c94aea8fe7efb0aefcc
Tatiana_Anuchina <1103atv@gmail.com>
2021-12-12 19:45:31 +1000

    a few code visualizaton changes

--- calculator.py
+++ calculator.py
@@ -53,7 +53,7 @@ elif type == "complex" or type == "Complex" or type == "COMPLEX":  # The endless
                     "Enter the symbol (+, -, *, /) or enter \"Stop\" for end>> ")
                 if text == "Stop" or text == "stop" or text == "STOP":
                     print("Total results is", total)
-                    break                
+                    break
                 next_number = float(input("Enter the next number>> "))
                 total -= next_number
                 print("Total is", total)
@@ -70,7 +70,7 @@ elif type == "complex" or type == "Complex" or type == "COMPLEX":  # The endless
                 next_number = float(input("Enter the next number>> "))
                 total *= next_number
                 print("Total is", total)
-            
+
         elif operation == "/":
             if second_number != 0:
                 total = first_number / second_number
@@ -87,13 +87,9 @@ elif type == "complex" or type == "Complex" or type == "COMPLEX":  # The endless
             else:
                 print("Cannot be divided by 0!")
 
-
         else:  # If wrong operation
             print("Error")
-    else:  # If not Stop and not Next
+    else:  # If not “Stop” and not “Next”
         print("Error")
-else:  # If not Simple and Complex
+else:  # If not “Simple” and not "Complex"
     print("Error")
-
-
-  
